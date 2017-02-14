a4f868acd1eca7f705b44eecb4d76301f4531487
Omar Hernandez <izonemarketing@Omars-MBP.fios-router.home>
2017-02-13 16:08:12 -0800

    added social media

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-/*
-$('ul.nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(100);
-}, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
-});
-*/
 // Offset for Main Navigation
 $('#custom-bootstrap-menu').affix({
     offset: {
