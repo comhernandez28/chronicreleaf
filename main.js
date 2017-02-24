fc69ebe8c74410f302f4a7c46c9e568de24b8e14
Omar Hernandez <izonemarketing@Omars-MBP.fios-router.home>
2017-02-24 14:57:32 -0800

    Swapped multiple images

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ $('#custom-bootstrap-menu').affix({
         top: 100
     }
 })
+
+$(function(){
+  $(window).scroll(function(){
+    var aTop = $('nav').height();
+    if($(this).scrollTop()>=aTop){
+        $('.navbar-toggle').addClass("navbar-scrolld");
+        $('.brand-logo').addClass("brand-logo-scrolld");
+    }
+  });
+});
+
+$(function(){
+  $(window).scroll(function(){
+    var aTop = $('nav').height();
+    if($(this).scrollTop()<=aTop){
+        $('.navbar-toggle').removeClass("navbar-scrolld");
+        $('.brand-logo').removeClass("brand-logo-scrolld");
+    }
+  });
+});
